249a3c597f8bca7a8e1ec98b7a686c52c5dbadbf
costantino-ventricelli <costantini.ventricelli@gmail.com>
2020-05-05 13:22:14 +0200

    Fixing LoadData and LoadDataTest

--- src/main/java/it/uniba/ventricellisardone/itss/load/LoadData.java
+++ src/main/java/it/uniba/ventricellisardone/itss/load/LoadData.java
@@ -1,34 +1,109 @@
 package it.uniba.ventricellisardone.itss.load;
 
-import com.google.cloud.bigquery.BigQuery;
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableSchema;
+import com.google.cloud.bigquery.*;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class LoadData {
 
-    private BigQuery bigQuery;
-    private Scanner fileReader;
-    private PrintWriter saveFile;
+    private final static String TAG = "LoadData.class";
+    private final static String DATASET = "dataset";
+    private final static String LOCATION = "eu";
+    private final static List<TableFieldSchema> SCHEMA;
 
-    public LoadData(String dataPath) throws FileNotFoundException{
-        readFile(dataPath);
+    static {
+        List<TableFieldSchema> arrayList = new ArrayList<>();
+        arrayList.add(new TableFieldSchema().setName("ordine_id_carrello").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_data").setName("DATE"));
+        arrayList.add(new TableFieldSchema().setName("ordine_giorno_nome").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_giorno_dell_anno").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_mese_nome").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_anno_valore").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_mese_valore").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_trimestre").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_periodo").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_trimestre_anno").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_mese_anno").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_feriale_non").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_festivo_non").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_codice_stato").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_stato_nome").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_sesso_acquirente").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_quantita").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_prezzo_pagato").setName("FLOAT"));
+        arrayList.add(new TableFieldSchema().setName("ordine_sconto").setName("INTEGER"));
+        arrayList.add(new TableFieldSchema().setName("ordine_outlet").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_brand").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_collezione").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_colore").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_sesso_articolo").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_metodo_pagamento").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_taglia").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_categoria").setName("STRING"));
+        arrayList.add(new TableFieldSchema().setName("ordine_macro_categoria").setName("STRING"));
+        SCHEMA = Collections.unmodifiableList(arrayList);
     }
 
-    public void startLoad(){
+    private final String dataDirectory;
+    private int actualLoad;
+    private final TableDataWriteChannel writer;
 
+    public LoadData(String dataDirectory, String tableName) throws IOException {
+        File directory = new File(dataDirectory);
+        if(!directory.exists())
+            throw new IOException("Directory not found");
+        else {
+            this.dataDirectory = dataDirectory;
+            BigQuery bigQuery = BigQueryOptions.getDefaultInstance().getService();
+            TableId tableId = TableId.of(DATASET, tableName);
+            WriteChannelConfiguration writeChannelConfiguration = WriteChannelConfiguration.newBuilder(tableId).setFormatOptions(FormatOptions.csv()).setAutodetect(true).build();
+            JobId jobId = JobId.newBuilder().setLocation(LOCATION).setProject("biproject-itss").build();
+            this.writer = bigQuery.writer(jobId, writeChannelConfiguration);
+        }
     }
 
-    private void executeQuery(String query){
-
+    public void startLoad(int startFrom, int endTo) throws IOException, InterruptedException {
+        this.actualLoad = startFrom;
+        saveOperation();
+        for(int i = startFrom; i < endTo; i++){
+            String file = new File(dataDirectory + "/load_data_" + i + ".csv").getPath();
+            System.out.println("[EXECUTING] File directory: " + file);
+            try (OutputStream stream = Channels.newOutputStream(writer)){
+                Files.copy(Paths.get(file), stream);
+            }
+            Job job = writer.getJob();
+            job = job.waitFor();
+            if(job.getStatus().getError() != null){
+                System.err.println("[EXECUTING] ERRORE: " + job.getStatus().getError());
+            }
+            JobStatistics.LoadStatistics statistics = job.getStatistics();
+            System.out.println("[EXECUTING] Output rows: " + statistics.getOutputRows());
+        }
     }
 
-    private void readFile(String dataPath) throws FileNotFoundException {
-
+    private void saveOperation() throws IOException {
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                                            .format(Calendar.getInstance().getTime())));
+        outputStream.writeInt(actualLoad);
+        outputStream.close();
     }
 
-    private void saveOperation(){
+    public int getLastLoad(Date date) throws IOException {
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                                        .format(date)));
+        int readInt = inputStream.readInt();
+        inputStream.close();
+        return readInt;
+    }
 
+    public int getActualLoad(){
+        return actualLoad;
     }
 }
--- src/test/java/it/uniba/ventricellisardone/itss/load/LoadDataTest.java
+++ src/test/java/it/uniba/ventricellisardone/itss/load/LoadDataTest.java
@@ -1,37 +1,68 @@
 package it.uniba.ventricellisardone.itss.load;
 
 import com.google.cloud.bigquery.*;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 public class LoadDataTest {
 
+    private static final Map<Integer, String> DWH_HEADER;
+    static {
+        Map<Integer, String> dwhHeader = new HashMap<>();
+        dwhHeader.put(0, "ordine_id_carrello");
+        dwhHeader.put(1, "ordine_data");
+        dwhHeader.put(2, "ordine_giorno_nome");
+        dwhHeader.put(3, "ordine_giorno_dell_anno");
+        dwhHeader.put(4, "ordine_mese_nome");
+        dwhHeader.put(5, "ordine_anno_valore");
+        dwhHeader.put(6, "ordine_mese_valore");
+        dwhHeader.put(7, "ordine_trimestre");
+        dwhHeader.put(8, "ordine_periodo");
+        dwhHeader.put(9, "ordine_trimestre_anno");
+        dwhHeader.put(10, "ordine_mese_anno");
+        dwhHeader.put(11, "ordine_feriale_non");
+        dwhHeader.put(12, "ordine_festivo_non");
+        dwhHeader.put(13, "ordine_codice_stato");
+        dwhHeader.put(14, "ordine_stato_nome");
+        dwhHeader.put(15, "ordine_sesso_acquirente");
+        dwhHeader.put(16, "ordine_quantita");
+        dwhHeader.put(17, "ordine_prezzo_pagato");
+        dwhHeader.put(18, "ordine_sconto");
+        dwhHeader.put(19, "ordine_outlet");
+        dwhHeader.put(20, "ordine_brand");
+        dwhHeader.put(21, "ordine_collezione");
+        dwhHeader.put(22, "ordine_colore");
+        dwhHeader.put(23, "ordine_sesso_articolo");
+        dwhHeader.put(24, "ordine_metodo_pagamento");
+        dwhHeader.put(25, "ordine_taglia");
+        dwhHeader.put(26, "ordine_categoria");
+        dwhHeader.put(27, "ordine_macro_categoria");
+        DWH_HEADER = Collections.unmodifiableMap(dwhHeader);
+    }
+
     @Test
-    public void rightConstructorTest() throws FileNotFoundException {
-        LoadData loadData = new LoadData(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("load_data.sql")).getPath());
+    public void rightConstructorTest() throws IOException {
+        new LoadData(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("load_data.sql")).getPath(), "test_tabella");
     }
 
     @Test
     public void wrongConstructorTest(){
-        Assertions.assertThrows(FileNotFoundException.class, () -> new LoadData("file_not_found.sql"), "File trovato inspiegabilmente");
+        Assertions.assertThrows(IOException.class, () -> new LoadData("file_not_found.sql", "test_tabella"), "File trovato inspiegabilmente");
     }
 
     @Test
-    public void completeLoadDataOnDWH() throws FileNotFoundException, InterruptedException {
-        LoadData loadData = new LoadData(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("load_data.sql")).getPath());
-        loadData.startLoad();
-        BigQuery bigQuery = BigQueryOptions.getDefaultInstance().getService();
+    public void completeLoadDataOnDWH() throws IOException, InterruptedException {
+        System.out.println("[EXECUTING] Test completo");
+        LoadData loadData = new LoadData(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("")).getPath(), "test_tabella");
+        loadData.startLoad(0, 1);
+        /*BigQuery bigQuery = BigQueryOptions.getDefaultInstance().getService();
         QueryJobConfiguration jobConfiguration = QueryJobConfiguration.newBuilder("SELECT * FROM `biproject-itss.dataset.test_tabella`;").build();
         JobId jobId = JobId.of(UUID.randomUUID().toString());
         Job job = bigQuery.create(JobInfo.newBuilder(jobConfiguration).setJobId(jobId).build());
@@ -42,20 +73,35 @@ public class LoadDataTest {
             throw new RuntimeException(job.getStatus().getError().toString());
 
         TableResult result = job.getQueryResults();
-        assert (result.getTotalRows() == 8) : "Numero di righe restitutito non corretto";
+        assert (result.getTotalRows() == 8) : "NUMERO RIGHE RESTITUITO NON CORRETTO";
         Scanner scanner = new Scanner(new File(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("data_result.json")).getPath()));
         String fileJSON = scanner.nextLine();
         JsonParser jsonParser = new JsonParser();
         JsonObject object = (JsonObject) jsonParser.parse(fileJSON);
         JsonArray array = (JsonArray) object.get("result");
         int i = 0;
+
         for(FieldValueList row : result.iterateAll()){
             JsonObject element = (JsonObject) array.get(i);
+            for (int j = 0; j < element.size(); j++) {
+                assert (row.get(j).getStringValue().equals(element.get(DWH_HEADER.get(j)).getAsString())) :
+                        "ERRORE ALL'ELEMENTO: " + i + " CAMPO: " + DWH_HEADER.get(j) + " DWH: " + row.get(j).getStringValue() + " JSON: " + element.get(DWH_HEADER.get(j)).getAsString();
+            }
+            i++;
         }
+
+        jobConfiguration = QueryJobConfiguration.newBuilder("DELETE * FROM `biproject-itss.dataset.test_tabella` WHERE TRUE;").build();
+        job = bigQuery.create(JobInfo.newBuilder(jobConfiguration).setJobId(jobId).build());
+        job.waitFor();
+         */
     }
 
+    /*
     @Test
-    public void interruptedLoadDataOnDWH(){
-
-    }
+    public void interruptedLoadDataOnDWH() throws IOException, InterruptedException {
+        System.out.println("[EXECUTING] Test interrotto");
+        LoadData loadData = new LoadData(Objects.requireNonNull(LoadDataTest.class.getClassLoader().getResource("")).getPath(), "test_table");
+        loadData.startLoad(0, 0);
+        assert (loadData.getLastLoad(Calendar.getInstance().getTime()) == 3) : "ERRORE NEL SALVATAGGIO DEL JOB";
+    }*/
 }
